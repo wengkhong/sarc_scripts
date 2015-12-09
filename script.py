04c270f6cf2a8cc11a917bf2ab3fb3a94d79a2f0
EC2 Default User <ec2-user@ip-172-31-5-171.ap-southeast-1.compute.internal>
2015-12-09 16:09:23 +0000

    Test

--- script.py
+++ script.py
@@ -8,3 +8,6 @@ if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa.gz"):
 	command = "aws s3 cp s3://takomaticsdata/reference_genomes/hg19/ /home/ec2-user/ref --recursive --exclude \"*\" --include \"hs37d5*\""
 	print(command)
 	call(command, shell = True)
+
+command = "docker pull wengkhong/speedseq"
+call(command, shell = True)
