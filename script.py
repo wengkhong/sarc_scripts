e17e7ce4d002463930b01d9538fafc9c81bf8c9e
EC2 Default User <ec2-user@ip-172-31-5-171.ap-southeast-1.compute.internal>
2015-12-09 16:05:21 +0000

    Test

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from subprocess import call
 print("Hello World")
 
 #Get reference sequences
-if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa"):
-	command = "aws s3 cp s3://takomaticsdata/reference_genomes/hg19/ ref --recursive --exclude \"*\" --include \"hs37d5*\""
+if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa.gz"):
+	command = "aws s3 cp s3://takomaticsdata/reference_genomes/hg19/ /home/ec2-user/ref --recursive --exclude \"*\" --include \"hs37d5*\""
 	print(command)
-	call(command)
+	call(command, shell = True)
