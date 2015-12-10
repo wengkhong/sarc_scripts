acd59ee1a35a01db706a4a8696182e18fef73054
EC2 Default User <ec2-user@ip-172-31-1-137.ap-southeast-1.compute.internal>
2015-12-10 10:30:54 +0000

    Test

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import os.path
 from subprocess import call
+import csv
+
 print("Hello World")
 
 #Get reference sequences
@@ -11,3 +13,18 @@ if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa.gz"):
 
 command = "docker pull wengkhong/speedseq"
 call(command, shell = True)
+
+call("sudo pip install boto3", shell = True)
+import boto3
+#Get sample list
+call("aws s3 cp s3://takomaticsdata/SarcomaPanel/Sarc_Samples.txt . ", shell = True)
+
+with open('Sarc_Samples.txt','r') as tsv:
+	sample_list = [line.strip().split('\t') for line in tsv]
+
+print("Length", len(sample_list))
+
+for line in sample_list:
+	#if line[0] == "File1":
+	#	continue
+	print(line[0])
