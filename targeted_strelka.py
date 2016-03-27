a9ad704b9cb5e1946b06f90023cd2bc8a240e819
Weng Khong Lim <Weng Khong Lim>
2016-03-28 00:59:07 +0800

    Signed-off-by: Weng Khong Lim <Weng Khong Lim>

--- targeted_strelka.py
+++ targeted_strelka.py
@@ -4,3 +4,25 @@ from subprocess import call
 import subprocess
 import csv
 import shutil
+
+#Get reference sequences
+if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa.gz"):
+        command = "aws s3 cp s3://takomaticsdata/reference_genomes/hg19/ /home/ec2-user/ref --recursive --exclude \"*\" --include \"hs37d5*\""
+        print(command)
+        call(command, shell = True)
+
+
+command = "docker pull wengkhong/speedseq-manual"
+call(command, shell = True)
+command = "docker pull wengkhong/vcflib"
+call(command, shell = True)
+
+
+def checkFileInS3(command):
+        proc = subprocess.Popen(command, stdout = subprocess.PIPE, shell = True)
+        (out,err) = proc.communicate()
+        #print out
+        if out:
+                return True;
+        if not out:
+                return False;
